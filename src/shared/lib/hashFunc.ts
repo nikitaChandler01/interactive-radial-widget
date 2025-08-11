b5b04fecebab6d43de98788e7bf596b9bfbf1b39
nikitaBerdnik <berdnik.nikita01@gmail.com>
2025-08-11 22:26:49 +0700

    refactor: hash fixed

--- src/shared/lib/hashFunc.ts
+++ src/shared/lib/hashFunc.ts
@@ -1,7 +1,7 @@
 export const getHash = (str: string) => {
   const prime = 31n;
   const seed = Math.floor(Math.random() * 1000);
-  let hash = BigInt(seed);
+  let hash = BigInt(seed + performance.now());
   for (let i = 0; i < str.length; i++) {
     const charCode = BigInt(str.charCodeAt(i));
     hash = hash * prime + charCode;
