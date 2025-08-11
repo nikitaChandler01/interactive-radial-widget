7f1df98791ddcdfa7adafacb5282d85a3fc1290d
nikitaBerdnik <berdnik.nikita01@gmail.com>
2025-08-11 20:12:08 +0700

    feat: vars reinited for scss

--- src/widgets/TimelineSelect/constants.ts
+++ src/widgets/TimelineSelect/constants.ts
@@ -1,24 +1,24 @@
 export const activeAndHoveredSelectStyles = {
   duration: 0.3,
-  ease: 'power2.out',
-  background: 'var(--bg-creamy)',
-  color: 'var(--font-color)',
+  ease: "power2.out",
+  background: " variables.$bg-creamy",
+  color: " variables.$font-color",
   scale: 8,
-  boxShadow: '0 0 0 0.125px #303E5880',
+  boxShadow: "0 0 0 0.125px #303E5880",
 };
 
 export const increasedSelectedItemStyles = {
   scale: 1,
   duration: 0.3,
-  background: 'var(--bg-creamy)',
-  ease: 'power2.out',
+  background: " variables.$bg-creamy",
+  ease: "power2.out",
 };
 
 export const decreasedSelectedItemStyles = {
   scale: 1,
   duration: 0.3,
-  background: 'var(--font-color)',
-  ease: 'power2.out',
+  background: " variables.$font-color",
+  ease: "power2.out",
 };
 
 export const rotationCircleDuration = 0.5;
